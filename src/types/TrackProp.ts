8a50f942accc3a8b9f1c18b42b19599afbe8198e
yosmisyael <yosmisyael7308@gmail.com>
2025-06-10 06:08:06 +0700

    feat: modify props

--- src/types/TrackProp.ts
+++ src/types/TrackProp.ts
@@ -1,14 +1,21 @@
-export interface AlbumProps {
+export interface PlaylistProps {
     id?: number;
     name?: string;
-    cover: string;
+    cover?: string;
     year?: number;
     color?: string;
+    songs: Song[];
 }
 
-export interface SongDetailProps {
-    artists: string[];
-    genre: string;
-    year: number;
-    likes: number;
+export interface Song {
+    id: number;
+    name: string;
+    year: number | null;
+    album: string | null;
+    artist: string | null;
+    duration_ms: number | null;
+    cover: string | null;
+    popularity: number | null;
+    preview_url: string | null;
+    filePath: string;
 }
